6bac7137fc371f3bcb4e8a91df59bf0cc97cc1ce
Rawan Bazzi <rawan.bazzi@net.usj.edu.lb>
2023-11-03 14:41:27 +0200

    Test

---
+++ Codes-React/lebconnect/src/aboutus.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './aboutus.css';
+
+function About() {
+    return (
+      <div className="about">
+        <h2>Our Mission</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          feugiat egestas nisi, vitae scelerisque justo consectetur ut.
+        </p>
+      </div>
+    );
+  }
+  
+  export default About;
