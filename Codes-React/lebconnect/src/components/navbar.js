d15dcf859a0cc720cc75ada4319148840182cc5e
anthonylazkani <anthonylazkani.22@gmail.com>
2023-11-12 16:54:41 +0200

    Navbar Logo corrected

--- Codes-React/lebconnect/src/components/navbar.js
+++ Codes-React/lebconnect/src/components/navbar.js
@@ -23,47 +23,44 @@ function Navbar(){
                 <header className='header-navbar'>
                     <div class='Logo'>
             
-                <Link to='/' className='navbar-logo'>
-               <h2> L<span>Connect</span></h2>
-                </Link>
-                <div className='menu-icon' onClick={hadleClick}>
-                    <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
-                </div>
-                </div>
-                <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-                    <li class='Navbar-links'>
-                        <Link to='/' className='Navbar-links' onClick={closeMobileMenu}>
-                            Home
+                        <Link to='/' className='navbar-logo'>
+                        <h2> L<span>Connect</span></h2>
                         </Link>
-                    </li>
-                    <li class='Navbar-links'>
-                        <Link to='/test' className='Navbar-links' onClick={closeMobileMenu}>
-                         News
-                        </Link>
-                    </li>
-                    <li class=' Navbar-links'>
-                        <Link to='/tourism' className='Navbar-links' onClick={closeMobileMenu}>
-                           Tourism
-                        </Link>
-                    </li>
-                    <li class='Navbar-links'>
-                        <Link to='/aboutus' className='Navbar-links' onClick={closeMobileMenu}>
-                           About us
-                        </Link>
-                    </li>
-                    <li class='Navbar-links'>
-                        <Link to='/Login' className='Navbar-links' onClick={closeMobileMenu}>
-                          Login
-                        </Link>
-                    </li>
-                    
-                    
-                </ul>
+                    <div className='menu-icon' onClick={hadleClick}>
+                        <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+                    </div>
+                    </div>
+                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                        <li class='Navbar-links'>
+                            <Link to='/' className='Navbar-links' onClick={closeMobileMenu}>
+                                Home
+                            </Link>
+                        </li>
+                        <li class='Navbar-links'>
+                            <Link to='/test' className='Navbar-links' onClick={closeMobileMenu}>
+                            News
+                            </Link>
+                        </li>
+                        <li class=' Navbar-links'>
+                            <Link to='/tourism' className='Navbar-links' onClick={closeMobileMenu}>
+                            Tourism
+                            </Link>
+                        </li>
+                        <li class='Navbar-links'>
+                            <Link to='/aboutus' className='Navbar-links' onClick={closeMobileMenu}>
+                            About us
+                            </Link>
+                        </li>
+                        <li class='Navbar-links'>
+                            <Link to='/Login' className='Navbar-links' onClick={closeMobileMenu}>
+                            Login
+                            </Link>
+                        </li>
+                        
+                        
+                    </ul>
                 </header>
-                <div class="Profile-Icon">
-                    <a href="#" class="active">
-                    <ion-icon name="person-outline"></ion-icon></a>
-      </div>
+                
             </nav>
         
         </>
