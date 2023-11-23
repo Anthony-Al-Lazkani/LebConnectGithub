32d09f1c719838e5c0319de9d43a9689c735e59f
anthonylazkani <anthonylazkani.22@gmail.com>
2023-11-23 04:09:50 +0200

    Profile done, contact us done

--- Codes-React/lebconnect/src/components/pages/Contact Us/contactus.js
+++ Codes-React/lebconnect/src/components/pages/Contact Us/contactus.js
@@ -1,8 +1,32 @@
 import React, { useState } from 'react';
 import './contactus.css';
 import axios from 'axios';
+import {useRef} from "react";
+import emailjs from '@emailjs/browser';
+import Popup from 'reactjs-popup';
+import {FaTimes} from 'react-icons/fa';
+
+
 
 export default function Contactus() {
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_n0db9oi', 'template_dmiw3vj', form.current, 'ZbwEKxZKV4IYHAhIa')
+      .then((result) => {
+          console.log(result.text);
+          setShowPopup(true);
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset();
+
+      console.log("Thank you for your Feedback!");
+  };
+    const [showPopup, setShowPopup] = useState(false);
     const [issue,setIssue]=useState('');
     const [message, setMessage] = useState('');
     const [email, setEmail] = useState('');
@@ -22,12 +46,16 @@ export default function Contactus() {
             setMessage('Something went wrong. Please try again later');
         }
     };
+
+    const closePopup = () => {
+      setShowPopup(false);
+    };
     
     return( 
         <div className="absolute mt-52 ml-48  
                         w-80 float-left border-2 p-2  
                         rounded-xl shadow-xl text-xl"> 
-           <form action='POST' onSubmit={handleSubmit}>
+           <form action='POST' onSubmit={handleSubmit} ref={form} onSubmit={sendEmail}>
                 <p className="text-2xl">Contact us Now !</p> 
                 <div> 
                 
@@ -53,13 +81,21 @@ export default function Contactus() {
   
                     </select> 
                     <br></br> 
+                    <label className="text-sm">Name*</label> 
+                    <br></br> 
+                    <input className="bg-gray-50 border border-gray-300  
+                                        text-sm rounded-lg focus:border-blue-500 
+                                        w-full p-2.5" 
+                            type="text" 
+                            placeholder="Name"  onChange={(e) => { setEmail(e.target.value) }} name='user_name'/> 
+                    <br></br> 
                     <label className="text-sm">Email Address*</label> 
                     <br></br> 
                     <input className="bg-gray-50 border border-gray-300  
                                         text-sm rounded-lg focus:border-blue-500 
                                         w-full p-2.5" 
                             type="email" 
-                            placeholder="Insert email here"  onChange={(e) => { setEmail(e.target.value) }}/> 
+                            placeholder="Insert email here"  onChange={(e) => { setEmail(e.target.value) }} name='user_email'/> 
                     <br></br> 
                     <label className="text-sm">Contact No.</label> 
                     <br></br> 
@@ -88,10 +124,17 @@ export default function Contactus() {
                                         py-2 px-4 rounded" 
                             type="submit"> 
                         Submit 
-                    </button> 
+                    </button>
+                    <Popup open={showPopup} closeOnDocumentClick onClose={closePopup}>
+                      <div className="modal">
+                        <div className="content">Thank You For Your Feedback!</div>
+                        <div>
+                          <button onClick={closePopup} className='Close-Popup'><FaTimes/></button>
+                        </div>
+                      </div>
+                  </Popup> 
                 </div> 
             </form> 
-            {message && <p className='Feedback-Message'>{message}</p>}
         </div> 
     ) 
 }
